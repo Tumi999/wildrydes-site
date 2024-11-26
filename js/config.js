3a1c8ae8d66bb0e3212528a4f4625b4e1ab4e9ea
Tumi999 <132591481+Tumi999@users.noreply.github.com>
2024-11-26 13:21:21 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_eD3Cwleul', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6tu35tqn7t8eslr71li5joiv6b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_oTQvZtkbS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2gnj7h2ad30o81usi8l5gbne8p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
